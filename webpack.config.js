5e2e531519039f6d37ec1b5d6e147ee954c146a3
masashi hiratsuka <hoop0911@icloud.com>
2019-06-17 15:34:02 +0900

    made sass file

--- webpack.config.js
+++ webpack.config.js
@@ -19,19 +19,6 @@ module.exports = {
           'css-loader',
           'sass-loader'
         ]
-      },
-      {
-       test: /\.(gif|png|jpg)$/,
-       use: [
-         {
-           loader: 'url-loader',
-           // 以下option部分がfile-loaderの読み込み(設定)
-           options: {
-           limit: 51200,// ＝51200B＝50KB
-           name: './img/[name].[ext]'
-           }
-         }
-       ]
       }
     ]
   },
